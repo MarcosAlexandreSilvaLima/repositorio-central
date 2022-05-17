06c424a6b9ae178a70119785b701cd7992bf50c6
MarcosAlexandreSilvaLima <01marcosalexandre@gmail.com>
2022-05-17 16:48:15 -0300

    dados de livro e ususario adicionando classe bibliotecario

--- src/biblioteca/Usuario.java
+++ src/biblioteca/Usuario.java
@@ -1,11 +1,18 @@
 package biblioteca;
 
 public class Usuario {
+	
+
+	private int id;
+	private String nome;
+	
+
 	public void cadastrar() {
 		// TODO Auto-generated method stub
 
 	}
 	
 	public void excluir() {}
+
 
 }
